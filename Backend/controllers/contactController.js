2fb30ffc289acf02715eae5a6bc7d99f0fdbc1fb
SyedKashanAbbasNaqvi <kashan@zikrainfotech.com>
2025-03-10 01:54:08 +0500

    backend

--- Backend/controllers/contactController.js
+++ Backend/controllers/contactController.js
@@ -1,88 +1,70 @@
 import Contactdata from "../models/contact.js";
 
-
-
 export const createContact = async (req, res) => {
-    try {
+  try {
+    const contact = new Contactdata(req.body);
 
-        const contact = new Contactdata(req.body);
-    
-        await contact.save();
-    
-        res.status(201).json(contact);
-    } catch (error) {
-       
-        res.status(500).json({ message: error.message });
-    }
+    await contact.save();
+
+    res.status(201).json(contact);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
 };
 
+export const getAllContacts = async (req, res) => {
+  try {
+    const contacts = await Contactdata.find();
+    res.status(200).json(contacts);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
 
+export const getContact = async (req, res) => {
+  try {
+    const individualcontact = await Contactdata.findById(req.params.id);
 
-export const getAllContacts = async (req,res)=>{
-    try{
-const  contacts = await Contactdata.find();
-res.status(200).json(contacts);
+    if (!individualcontact) {
+      return res.status(404).json({ message: "Contact not Forund" });
+    } else {
+      res.status(200).json(individualcontact);
     }
-    catch(error){
-        res.status(500).json({ message: error.message });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
 
+export const deleteContact = async (req, res) => {
+  try {
+    const contact = await Contactdata.findByIdAndDelete(req.params.id);
+
+    if (!contact) {
+      return res.status(404).json({ message: "Contact not Found" });
+    } else {
+      res.status(200).json({ message: "Contact Deleted Sucessfully" });
     }
-}
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
 
-export const getContact = async (req,res)=>{
-    try{
-const  individualcontact = await Contactdata.findById(req.params.id);
+export const updateContact = async (req, res) => {
+  try {
+    const updatedcontact = await Contactdata.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      {
+        new: true,
+      }
+    );
 
-if(!individualcontact){
-
-    return res.status(404).json({message:"Contact not Forund"})
-}else{
-    res.status(200).json(individualcontact);
-}
-
+    if (!updatedcontact) {
+      return res.status(404).json({ message: "Contact not Found" });
+    } else {
+      res.status(200).json(updatedcontact);
     }
-    catch(error){
-        res.status(500).json({ message: error.message });
-
-    }
-}
-
-
-export const deleteContact  = async (req,res)=>{
-    try{
-const  contact = await Contactdata.findByIdAndDelete(req.params.id);
-
-if(!contact){
-
-    return res.status(404).json({message:"Contact not Found"})
-}else{
-    res.status(200).json({message :"Contact Deleted Sucessfully"});
-}
-
-    }
-    catch(error){
-        res.status(500).json({ message: error.message });
-
-    }
-}
-
-
-export const updateContact  = async (req,res)=>{
-    try{
-const  updatedcontact = await Contactdata.findByIdAndUpdate(req.params.id,req.body,{
-    new :true,
-});
-
-if(!updatedcontact){
-
-    return res.status(404).json({message:"Contact not Found"})
-}else{
-    res.status(200).json(updatedcontact);
-}
-
-    }
-    catch(error){
-        res.status(500).json({ message: error.message });
-
-    }
-}
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
--- Backend/models/contact.js
+++ Backend/models/contact.js
@@ -1,38 +1,35 @@
 import mongoose, { Schema } from "mongoose";
 
-const  usercontactdata = new Schema({
+const usercontactdata = new Schema({
+  name: {
+    type: String,
+    required: true,
+  },
 
+  email: {
+    type: String,
+    required: true,
+  },
 
+  phone_no: {
+    type: String,
+    required: true,
+  },
+  subject: {
+    type: String,
+    required: true,
+  },
 
-    name:{
-        type:String,
-        required:true
-    },
+  message: {
+    type: String,
+    required: true,
+  },
 
-    email:{
-        type:String,
-        required:true
-    },
-
-    phone_no:{
-        type:String,
-        required:true
-    }, 
-       subject:{
-        type:String,
-        required:true
-    },  
-    
-    message:{
-        type:String,
-        required:true
-    },
-
-    date:{
-        type:Date,
-      default:Date.now()
-    },
-})
+  date: {
+    type: Date,
+    default: Date.now(),
+  },
+});
 
 const Contactdata = mongoose.model("Contact", usercontactdata);
 
